1ce6e0b370c8ca800e369882460e7161d6e1e09f
iganna <igolkinaanna11@gmail.com>
2023-10-07 13:01:11 +0200

    major synteny

--- synteny_majoir.R
+++ synteny_majoir.R
@@ -97,11 +97,11 @@ for(i.query in 1:length(query.name)){
 # print(chromosome.pairs)
 
 
-# for.flag = F
-# tmp = foreach(i.chr.pair = 1:nrow(chromosome.pairs), .packages=c('stringr','Biostrings', 'seqinr', 'spgs'))  %dopar% {  # which accession to use
+for.flag = F
+tmp = foreach(i.chr.pair = 1:nrow(chromosome.pairs), .packages=c('stringr','Biostrings', 'seqinr', 'spgs'))  %dopar% {  # which accession to use
   
-for.flag = T
-for(i.chr.pair in 1:nrow(chromosome.pairs)){
+# for.flag = T
+# for(i.chr.pair in 1:nrow(chromosome.pairs)){
   
   
   
