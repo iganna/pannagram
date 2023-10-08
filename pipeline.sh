58c92371ed0cd71cb8b91b142da6227eefb3b42c
iganna <igolkinaanna11@gmail.com>
2023-10-08 19:11:47 +0200

    new blast of baps - done

--- pipeline.sh
+++ pipeline.sh
@@ -164,7 +164,7 @@ ref_pref=${ref_pref//_/$'-'}
 Rscript synteny_majoir.R --path.blast ../${pref_global}_blast_res_${ref_pref}/ --path.aln ../${pref_global}_alignments_${ref_pref}/ \
 --type fasta --pref ${ref_pref} --path.ref  ${path_chr_ref}  \
 --path.gaps ../${pref_global}_gaps_${ref_pref}/  --path.query ${path_chr_acc} \
---n.chr.ref ${n_chr_ref} --n.chr.acc ${n_chr_query}  --all.vs.all ${all_cmp}
+--n.chr.ref ${n_chr_ref} --n.chr.acc ${n_chr_query}  --all.vs.all ${all_cmp} -c ${cores}
 
 # # If the first round of alignment didn't have any errors - remove the blast which was needed for it
 # rm -rf ../${pref_global}_blast_res_${ref_pref}/
