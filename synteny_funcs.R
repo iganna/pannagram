15c0b92b176db8ac645af77c32f949dd0c2ca5c3
iganna <igolkinaanna11@gmail.com>
2023-10-08 11:06:34 +0200

    synteny

--- synteny_funcs.R
+++ synteny_funcs.R
@@ -91,7 +91,7 @@ removeSmallOverlapps <- function(x.sk, rm.threshold = 0.5){
       x.sk$V9[irow] = substr(x.sk$V9[irow], (aln.adjust+1), nchar(x.sk$V9[irow]))
       
       # Adjust positions
-      s.q.cut = str2nt(substr(x.sk$V8[irow], 1, aln.adjust))
+      s.q.cut = seq2nt(substr(x.sk$V8[irow], 1, aln.adjust))
       adjustment.q = sum(s.q.cut != '-')
       x.sk$V2[irow] = x.sk$V2[irow] + adjustment.q * sing(adjustment)
       x.sk$V4[irow] = x.sk$V4[irow] + adjustment.dir
@@ -109,7 +109,7 @@ removeSmallOverlapps <- function(x.sk, rm.threshold = 0.5){
       
       # Adjust positions
       
-      s.q.cut = str2nt(substr(x.sk$V8[irow], (aln.adjust), nchar(x.sk$V8[irow])))
+      s.q.cut = seq2nt(substr(x.sk$V8[irow], (aln.adjust), nchar(x.sk$V8[irow])))
       adjustment.q = sum(s.q.cut != '-') 
       x.sk$V3[irow] = x.sk$V3[irow] + adjustment.q * sing(adjustment)
       x.sk$V5[irow] = x.sk$V5[irow] + adjustment.dir
