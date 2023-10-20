1ad4aef6415908036eaa645ccc9694f689899272
iganna <igolkinaanna11@gmail.com>
2023-10-20 15:55:28 +0200

    between gaps

--- pipeline.sh
+++ pipeline.sh
@@ -173,7 +173,7 @@ ref_pref=${ref_pref//_/$'-'}
 # ./blast_gaps.sh -path_gaps ../${pref_global}_gaps_${ref_pref}/ 
 
 # # Second round of alignments
-Rscript synteny_gap.R --path.blast ../${pref_global}_blast_res_${ref_pref}/ --path.aln ../${pref_global}_alignments_${ref_pref}/ \
+Rscript synteny_gaps.R --path.blast ../${pref_global}_blast_res_${ref_pref}/ --path.aln ../${pref_global}_alignments_${ref_pref}/ \
 --type fasta --pref ${ref_pref} --path.ref  ${path_chr_ref}  \
 --path.gaps ../${pref_global}_gaps_${ref_pref}/  --path.query ${path_chr_acc} \
 --n.chr.ref ${n_chr_ref} --n.chr.acc ${n_chr_query}  --all.vs.all ${all_cmp} -c ${cores}
