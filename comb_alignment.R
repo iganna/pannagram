28b9ad8269dc72d2b76de76ab2eb4e26a92a856b
iganna <igolkinaanna11@gmail.com>
2023-10-22 11:59:01 +0200

    combo

--- comb_alignment.R
+++ comb_alignment.R
@@ -1,102 +1,86 @@
-library('foreach')
-library(doParallel)
-library("optparse")
-source("synteny_infer.R")
+suppressMessages({
+  library(foreach)
+  library(doParallel)
+  library(optparse)
+  library(crayon)
+})
+
+
+
+source("utils.R")
+source("synteny_funcs.R")
+
+pokazStage('Combine: alignments by chromosomes')
 
 args = commandArgs(trailingOnly=TRUE)
 
 
 option_list = list(
-  make_option(c("--path.work"), type="character", default=NULL, 
-              help="path to working directory", metavar="character"),
-  make_option(c("--path.base"), type="character", default=NULL, 
-              help="path to base chromosomes", metavar="character"),
-  make_option(c("--path.aln.pref"), type="character", default=NULL, 
-              help="path with alignments", metavar="character"),
-  
-  make_option(c("--file.chr.len.ref"), type="character", default=NULL, 
-              help="file with lengths of chromosomes of reference accessions", metavar="character"),
-  make_option(c("--n.cores"), type="character", default=NULL, 
-              help="numer of cores: 10 max", metavar="character"),
-  
-  make_option(c("--accs"), type="character", default=NULL, 
-              help="accessions to create the alignment", metavar="character"),
-  
-  
-  make_option(c("--n.chr.ref"), type="character", default=NULL, 
+  make_option(c("--path.chr.len"), type="character", default=NULL, 
+              help="file with lengths of chromosomes", metavar="character"),
+  make_option(c("--path.cons"), type="character", default=NULL, 
+              help="path to consensus directory", metavar="character"),
+  make_option(c("-o", "--path.aln"), type="character", default=NULL, 
+              help="path to the output directory with alignments", metavar="character"),
+  make_option(c("-p", "--pref"), type="character", default=NULL, 
+              help="prefix of the reference file", metavar="character"),
+  make_option(c("-n", "--n.chr.ref"), type="character", default=NULL, 
               help="number of chromosomes in the reference genome", metavar="character"),
-  make_option(c("--n.chr.acc"), type="character", default=NULL, 
+  make_option(c("-m", "--n.chr.acc"), type="character", default=NULL, 
               help="number of chromosomes in the accessions", metavar="character"),
-  
-  make_option(c("--ref.acc"), type="character", default=NULL, 
-              help="reference accessions", metavar="character"),
-  
-  make_option(c("--all.vs.all"), type="character", default=NULL, 
-              help="alignment of all chromosomes vs all or not: T/F", metavar="character")
-  
+  make_option(c("-a", "--all.vs.all"), type="character", default=NULL, 
+              help="alignment of all chromosomes vs all or not: T/F", metavar="character"),
+  make_option(c("-c", "--cores"), type = "integer", default = 1, 
+              help = "number of cores to use for parallel processing", metavar = "integer"),
+  make_option(c("-r", "--path.ref"), type="character", default=NULL, 
+              help="path to the reference file", metavar="character"),
+  make_option(c("-t", "--type"), type="character", default=NULL, 
+              help="type of fasta files", metavar="character"),
 ); 
+
 
 opt_parser = OptionParser(option_list=option_list);
 opt = parse_args(opt_parser, args = args);
 
-print(opt)
-# return()
+# print(opt)
 
-if (!is.null(opt$path.work)) path.work <- opt$path.work
+# Set the number of cores for parallel processing
+num.cores.max = 10
+num.cores <- min(num.cores.max, ifelse(!is.null(opt$cores), opt$cores, num.cores.max))
+myCluster <- makeCluster(num.cores, type = "PSOCK")
+registerDoParallel(myCluster)
 
-if (!is.null(opt$path.base)) path.base <- opt$path.base
+# Path with the consensus output
+if (!is.null(opt$path.cons)) path.cons <- opt$path.cons
+if(!dir.exists(path.cons)) system(paste('mkdir ', path.cons, sep = ''))
+
+# File with chromosomal lengths, which should be in the consensus dir
+if (!is.null(opt$path.chr.len)) path.chr.len <- opt$path.chr.len
+path.chr.len = paste(path.cons, path.chr.len, sep = '')
+if(!dir.exists(path.chr.len)) system(paste('mkdir ', path.chr.len, sep = ''))
+
+if (!is.null(opt$path.ref)) path.base <- opt$path.ref  # to know the chromosomal lengths
+if (!is.null(opt$type)) base.suff <- opt$type  # to read fasta file
 if (!is.null(opt$ref.acc)) base.acc.ref <- opt$ref.acc
 
+# Path with alignments
 if (!is.null(opt$path.aln.pref)) path.pref <- opt$path.aln.pref
 
-if (!is.null(opt$file.chr.len.ref)) file.chr.len.ref <- opt$file.chr.len.ref
-
+# To create combinations
 if (!is.null(opt$n.chr.ref)) n.chr.ref <- opt$n.chr.ref
 if (!is.null(opt$n.chr.acc)) n.chr.acc <- opt$n.chr.acc
 if (!is.null(opt$all.vs.all)) all.vs.all <- as.logical(opt$all.vs.all)
 
-accs <- opt$accs
 
-if (!is.null(opt$n.cores)) {
-  n.cores <- min(10, as.numeric(opt$n.cores))
-} else {
-  n.cores = 10
-}
+# ---- Get accession names ----
+aln.suff = '_maj.rds'
+aln.files <- list.files(path = path.aln, 
+                        pattern = sub("\\.rds", "\\\\.rds$", aln.suff))
+accessions <- unique(sub("_(.*)", "", aln.files))
 
-# ===========================================================================
-# ===========================================================================
+pokaz('Accessions:', accessions)
 
-# 10 CORES MAXIMUM!!!!!!!!!!!
-myCluster <- makeCluster(n.cores, # number of cores to use           # 10 CORES MAXIMUM!!!!!!!!!!!
-                         type = "PSOCK") # type of cluster
-registerDoParallel(myCluster)
-
-# ------------------------------------------------------------------
-
-# acc.ref = c('0', '10002', '10015','10024', '1741', '22005', '6046', '6124', '6244', '8236', '9537') 
-# 0,10002,10015,10024,1741,22005,6046,6124,6244,8236,9537
-print(path.pref)
-
-path.aln.ref =  paste(path.pref, base.acc.ref, '/', sep = '')
-
-# ------------------------------------------------------------------
-# Get accession names
-aln.pattern = '_postgap3.rds'
-accessions = c()
-for(p in path.aln.ref){
-  accessions = c(accessions, unique(sapply(list.files(p, pattern=paste('*', aln.pattern, sep = '')), function(s) strsplit(s, '_')[[1]][1])))
-}
-acc.tbl = table(accessions)
-accessions = names(acc.tbl)
-
-if(!is.null(accs)){
-  accessions = intersect(accessions, acc.tbl)
-}
-
-print(accessions)
-
-# -------------------
-# Combinations of chromosomes query-base to create the alignments
+# ---- Combinations of chromosomes query-base to create the alignments ----
 chromosome.pairs = c()
 for(i.query in 1:length(accessions)){
   for(query.chr in 1:n.chr.acc){
@@ -112,34 +96,30 @@ for(i.query in 1:length(accessions)){
 }
 chromosome.pairs = unique(chromosome.pairs[,c(2,3)])
 
-# -------------------
-# Length of reference chromosomes
-if(!file.exists(file.chr.len.ref)){
-  chr.len = c()
+# ---- Length of reference chromosomes ----
+
+file.chr.len = paste(path.chr.len, base.acc.ref, '_len.rds', sep = '')
+if(!file.exists(file.chr.len)){
+  for(i.chr in 1:n.chr.ref){
+    acc = base.acc.ref
+    # print(c(i.chr, acc))
+    
+    # Read base chromosome
+    base.file = paste0(base.acc.ref, '_chr', base.chr , '.', base.suff, collapse = '')
+    pokaz('Base:', base.file)
+    base.fas.fw = readFastaMy(paste(path.base, base.file, sep = ''))
+    
+    chr.len = c(chr.len, length(base.fas.fw))
+  }
+  saveRDS(chr.len, file.chr.len)
 } else {
-  chr.len = readRDS(file.chr.len.ref)
+  chr.len = readRDS(file.chr.len)
 }
 
-add.flag = F
-for(i.chr in 1:n.chr.ref){
-  acc = base.acc.ref
-  # print(c(i.chr, acc))
-  if(!is.null(chr.len)){
-    idx = (chr.len$acc == acc) & (chr.len$chr == i.chr)
-    if(sum(idx) > 0) next
-  }
-  add.flag = T
-  g = seqinr::read.fasta(paste(path.base, acc, '_chr', i.chr, '.fasta', sep = ''))[[1]]
-  chr.len = rbind(chr.len, data.frame(acc = acc, chr = i.chr, len = length(g)))
-}
-if(add.flag) saveRDS(chr.len, file.chr.len.ref)
 
 # ------------------------------------------------------------------
 
-print(base.acc.ref)
-print(chromosome.pairs)
-
-if(!dir.exists(path.work)) system(paste('mkdir ', path.work, sep = ''))
+pokaz('Reference:', base.acc.ref)
 
 # flag.for = F
 # tmp = foreach(i.chr.pair = 1:nrow(chromosome.pairs))  %dopar% {  # which accession to use
@@ -149,38 +129,44 @@ for(i.chr.pair in 1:nrow(chromosome.pairs)){
   query.chr = chromosome.pairs[i.chr.pair, 1]
   base.chr = chromosome.pairs[i.chr.pair, 2]
   
-  val.all = c()
-  val.names = c()
   for(acc in accessions){
-    print(acc)
     
-    f.acc2ref = paste(path.aln.ref, acc, '_', query.chr, '_', base.chr, aln.pattern, sep = '')
-    if(!file.exists(f.acc2ref)) next
-    t.acc2ref = readRDS(f.acc2ref)
-    base.len.ref = chr.len[(chr.len[,1] == base.acc.ref) & (chr.len[,2] == base.chr),3]
-    val = getCorresp2BaseSign(t.acc2ref, base.len.ref)
-    rm(t.acc2ref)
+    pref.comb = paste0(acc, '_', query.chr, '_', base.chr, collapse = '')
+    file.aln.full <- paste(path.aln, paste0(pref.comb,  '_full.rds', collapse = ''), sep = '')
+    if(!file.exists(file.aln.full)) next
+    pokaz('Aln exists:', file.aln.full)
     
-    val.all = cbind(val.all, val)
-    val.names = c(val.names, acc)
-    rm(val)
+    # 
+    # 
+    # print(acc)
+    # 
+    # f.acc2ref = paste(path.aln.ref, acc, '_', query.chr, '_', base.chr, aln.suff, sep = '')
+    # if(!file.exists(f.acc2ref)) next
+    # t.acc2ref = readRDS(f.acc2ref)
+    # base.len.ref = chr.len[base.chr]
+    # val = getCorresp2BaseSign(t.acc2ref, base.len.ref)
+    # rm(t.acc2ref)
+    # 
+    # val.all = cbind(val.all, val)
+    # val.names = c(val.names, acc)
+    # rm(val)
   }
-  
-  if(length(val.names) == 0){
-    if(flag.for){
-      next
-    } else {
-      return(NULL)  
-    }
-  }
-  colnames(val.all) = val.names
-  val.all = cbind(1:nrow(val.all),val.all)
-  colnames(val.all)[1] = base.acc.ref
-  
-  file.out.consensus = paste(path.work, 'consensus_', query.chr, '_', base.chr, '_', base.acc.ref, '_direct.rds', sep = '')
-  saveRDS(val.all, file.out.consensus, compress = F)
-  rm(val.all)
-  gc()
+  # 
+  # if(length(val.names) == 0){
+  #   if(flag.for){
+  #     next
+  #   } else {
+  #     return(NULL)  
+  #   }
+  # }
+  # colnames(val.all) = val.names
+  # val.all = cbind(1:nrow(val.all),val.all)
+  # colnames(val.all)[1] = base.acc.ref
+  # 
+  # file.out.consensus = paste(path.cons, 'consensus_', query.chr, '_', base.chr, '_', base.acc.ref, '_direct.rds', sep = '')
+  # saveRDS(val.all, file.out.consensus, compress = F)
+  # rm(val.all)
+  # gc()
   
 }
 
