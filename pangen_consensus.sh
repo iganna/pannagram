06c7cfc15778cdc08b277aa8807f9217662e77f0
iganna <igolkinaanna11@gmail.com>
2023-12-10 15:07:47 +0100

    stages fix

--- pangen_consensus.sh
+++ pangen_consensus.sh
@@ -179,13 +179,12 @@ if [ -z "${start_step}" ]; then
 fi
 
 # Looping through and deleting files of stages, which are less then the current one
+echo "${path_flags}step*_done*"
 for file_step in "${path_flags}step*_done*"; do
 
-    echo "check ${file_step}"
     if [ -f "$file_step" ]; then
         # Extracting step number from the file name
         step_tmp=$(echo "$file_step" | sed -e 's/.*step\([0-9]*\)_done.*/\1/')
-        echo ${step_tmp}
 
         # Check if step number is greater or equal to start_step
         if [ "$step_tmp" -ge "$start_step" ]; then
