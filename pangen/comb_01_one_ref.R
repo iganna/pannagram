a67c00804534353890dd9c2fa3a467b09e7ca6ac
iganna <igolkinaanna11@gmail.com>
2023-12-15 17:24:22 +0100

    ref-based

--- pangen/comb_01_one_ref.R
+++ pangen/comb_01_one_ref.R
@@ -134,10 +134,10 @@ pokaz('Reference:', base.acc.ref)
 
 max.len.gap = 20000
 
-#flag.for = F
-#tmp = foreach(i.chr.pair = 1:nrow(chromosome.pairs), .packages=c('rhdf5', 'crayon'))  %dopar% {  # which accession to use
- flag.for = T
- for(i.chr.pair in 1:nrow(chromosome.pairs)){
+flag.for = F
+tmp = foreach(i.chr.pair = 1:nrow(chromosome.pairs), .packages=c('rhdf5', 'crayon'))  %dopar% {  # which accession to use
+ # flag.for = T
+ # for(i.chr.pair in 1:nrow(chromosome.pairs)){
   
   query.chr = chromosome.pairs[i.chr.pair, 1]
   base.chr = chromosome.pairs[i.chr.pair, 2]
