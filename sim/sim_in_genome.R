e1197653bee6ddc74fb9a0768c74c4c22b691eda
Анна Иголкина <annaigolkina@Annas-MacBook-Pro.local>
2024-02-19 20:56:14 +0100

    explicit counts

--- sim/sim_in_genome.R
+++ sim/sim_in_genome.R
@@ -70,7 +70,7 @@ write.table(res, paste(output.file, '.table', sep = ''), quote = F, row.names = 
 res.cnt =as.data.frame.matrix(table(res$V1, res$V8))
 res.cnt$total = rowSums(res.cnt)
 res.cnt = res.cnt[order(-res.cnt$total),]
-write.table(res.cnt, paste(output.file, '.cnt', sep = ''), quote = F, row.names = F, col.names = T, sep = '\t')
+write.table(res.cnt, paste(output.file, '.cnt', sep = ''), quote = F, row.names = T, col.names = T, sep = '\t')
 
 cnt = tapply(res$V8, res$V1, length)
 pokaz('Mean, min, max number of hits per sequence:', mean(cnt),  min(cnt),  max(cnt))
