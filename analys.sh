58bcf9960e3a270b4dee169dc131200c6708ce11
Анна Иголкина <annaigolkina@Annas-MacBook-Pro.local>
2024-03-01 20:28:39 +0100

    prefix of the alignment

--- analys.sh
+++ analys.sh
@@ -46,6 +46,7 @@ run_blocks=false
 run_seq=false
 run_aln=false
 run_snp=false
+aln_type='msa_'
 # run_sv=false
 
 # Parse command line arguments
@@ -53,6 +54,7 @@ while [ $# -gt 0 ]; do
     case $1 in
         -pref_global) pref_global=$2; shift 2;;
         -ref_pref) ref_pref=$2; shift 2;;
+        -aln_type) aln_type=$2; shift 2;;
         -path_consensus) path_consensus=$2; shift 2;;
         -path_chromosomes) path_chromosomes=$2; shift 2 ;;
         -cores) cores=$2; shift 2 ;;
@@ -87,7 +89,7 @@ path_chromosomes=$(add_symbol_if_missing "$path_chromosomes" "/")
 # Execute scripts based on the provided keys
 if [ "$run_blocks" = true ]; then
 
-    Rscript analys/analys_01_blocks.R --path.cons ${path_consensus} --ref.pref  ${ref_pref} --cores ${cores}
+    Rscript analys/analys_01_blocks.R --path.cons ${path_consensus} --ref.pref  ${ref_pref} --cores ${cores} --aln.type ${aln_type}
 fi
 
 if [ "$run_seq" = true ]; then
