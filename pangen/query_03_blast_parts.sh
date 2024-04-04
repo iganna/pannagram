705c19e6e5300b51b4e28094e623103cfc3126b0
Анна Иголкина <annaigolkina@Annas-MacBook-Pro.local>
2024-04-04 17:40:43 +0200

    test

--- pangen/query_03_blast_parts.sh
+++ pangen/query_03_blast_parts.sh
@@ -108,6 +108,7 @@ run_blast() {
     ref_chr=${r_filename##*chr}
 
     echo "${part_chr} ${ref_chr}"
+    echo "${p_prefix} ${r_prefix}"
 
     if [[ "$p_prefix" == "$r_prefix" ]] || { [[ "$part_chr" != "$ref_chr" ]] && [[ ${all_vs_all} == "F" ]]; } || [[ -f "$outfile" ]]; then
         return
