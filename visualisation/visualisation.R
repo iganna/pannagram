a5d33845bb8732edcfc5d63ecaad42bc081a181e
Alex <curjucklloyd@gmail.com>
2024-04-26 15:45:39 +0300

    new `plotGenomeAgainstRef` function with docstrings

--- visualisation/visualisation.R
+++ visualisation/visualisation.R
@@ -118,4 +118,94 @@ plotPanAcc <- function(file.msa, acc){
   
 }
 
-
+#' ----------------------------------------------------------------------
+#' Plot Synteny Between Two Genomes
+#'
+#' This function wraps `plotSynteny` to visualize synteny regions between two full genomes, reading the files, produced by the pangen pipeline
+#'
+#' @param alignments.path Char with path to **directory**, where `.rds` files with alignments are located. By default, pangen pipeline writes such files into dir with **alignments_** prefix
+#' @param query.name Char with path to **FASTA file** with genome, used as query (horizontal axis)
+#' @param ref.name Char with path to **FASTA file** with genome, used as reference (vertical axis)
+#' @param sort.descending Flag to sort sequences (chromosome, plasmids, fragments, etc.) of a genome by their lengths in descending order. Default: `TRUE`
+#' @param query.label Char for x axis label (query). Default `NULL` will automatically deduce label based on given `query.name`
+#' @param ref.label Char for y axis label (reference). Default `NULL` will automatically deduce label based on given `ref.name`
+#'
+#' @return A `ggplot2` plot object.
+#'
+#' @import ggplot2
+#' @export
+plotGenomeAgainstRef <- function(alignments.path, query.name, ref.name,
+                                 sort.descending=T,
+                                 query.label = NULL,
+                                 ref.label = NULL) {
+  # if none given, deducing axis labels straight from filenames
+  if (is.null(query.label)) query.label = tools::file_path_sans_ext(basename(query.name))
+  if (is.null(ref.label)) ref.label = tools::file_path_sans_ext(basename(ref.name))
+  
+  # Read FASTA files
+  fasta_query <- readFastaMy(query.name)
+  fasta_ref <- readFastaMy(ref.name)
+  
+  # =============== reordering ===================
+  if (sort.descending){
+    order_query <- order(-nchar(fasta_query))
+    order_ref <- order(-nchar(fasta_ref))
+    fasta_query <- fasta_query[order_query]
+    fasta_ref <- fasta_ref[order_ref]
+  } else {
+    order_query <- seq(1, length(nchar(fasta_query)))
+    order_ref <- seq(1, length(nchar(fasta_ref)))
+  }
+  
+  len_query = nchar(fasta_query)
+  len_ref = nchar(fasta_ref)
+  cum_query = c(0, cumsum(len_query))
+  cum_ref = c(0, cumsum(len_ref))
+  
+  
+  #=========== Filling the new dataframe =====================
+  df <- data.frame()
+  query_prefix <- tools::file_path_sans_ext(basename(query.name))
+  for (i in seq_along(fasta_query)) {
+    for (j in seq_along(fasta_ref)) {
+      
+      file_name = paste(query_prefix, "_", order_query[i], "_", order_ref[j], "_maj.rds", sep='')
+      file_path = file.path(alignments.path, file_name)
+      if (file.exists(file_path)) {
+        data_ij <- readRDS(file_path)
+        
+        data_ij[, c(2, 3)] = data_ij[, c(2, 3)] + cum_query[i]
+        data_ij[, c(4, 5)] = data_ij[, c(4, 5)] + cum_ref[j]
+        df = rbind(df, data_ij)
+      }
+    }
+  }
+  
+  v.plasmid.divisors = cum_query[-length(cum_query)]
+  h.plasmid.divisors = cum_ref[-length(cum_ref)]
+  
+  query_labels = sub("^[^ ]+ ", "", names(fasta_query))
+  ref_labels = sub("^[^ ]+ ", "", names(fasta_ref))
+  
+  pS = plotSynteny(df,
+                   query.label=query.label,
+                   ref.label=ref.label,
+                   axis.ticks=seq(0, 10, by = 2),
+                   hlines=h.plasmid.divisors,
+                   vlines=v.plasmid.divisors,
+                   col.line = "#3530D966",
+                   show.point = T,
+                   expand = c(0,0)
+  ) +
+    annotate("text", x = cum_query[-1], y = rep(0, length(cum_query) - 1),
+             label = query_labels,
+             vjust = -0.3, hjust = -0.02,
+             size = 2.7, angle=90,
+             color="#7D7D7D") +
+    annotate("text", x = rep(0, length(cum_ref) - 1), y = cum_ref[-1], 
+             label = ref_labels,
+             vjust = 1.2, hjust = -0.02,
+             size = 2.7,
+             color="#7D7D7D")
+  return(pS)
+}
