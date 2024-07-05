2e4bef9c627736c89f7991c4bae48f9368f11dfc
Анна Иголкина <annaigolkina@Annas-MacBook-Pro.local>
2024-07-05 14:53:17 +0200

    error handling block done

--- analys.sh
+++ analys.sh
@@ -4,22 +4,7 @@
 #            ERROR HANDLING BLOCK
 # ----------------------------------------------------------------------------
 
-# Exit immediately if any command returns a non-zero status
-set -e
-
-# Keep track of the last executed command
-trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
-
-# Define a trap for the EXIT signal
-trap 'catch $?' EXIT
-
-# Function to handle the exit signal
-catch() {
-    # Check if the exit code is non-zero
-    if [ $1 -ne 0 ]; then
-        echo "\"${last_command}\" command failed with exit code $1."
-    fi
-}
+source utils/error_block.sh
 
 # ----------------------------------------------------------------------------
 #             FUNCTIONS
