1c692c35da8ffc0c79cbd1a3e7f24944742bb690
Alex <curjucklloyd@gmail.com>
2024-07-05 14:36:05 +0300

    tests update (for utils/utils.R): - readFastaMy - writeFastaMy - seq2int - nt2seq - splitSeq - aln2mx - mx2aln - mx2seq

---
+++ tests/testthat/seq2int.R
@@ -0,0 +1,21 @@
+library(testthat)
+source("../../utils/utils.R")
+
+
+test_that("seq2nt: Couldn't convert string to vector of nucleotides", {
+  result <- seq2nt("ATGC")
+  expect_type(result, "character")
+  expect_length(result, 4)
+  expect_equal(result, c("A", "T", "G", "C"))
+})
+
+test_that("seq2nt: Couldn't handle empty string", {
+  result <- seq2nt("")
+  expect_type(result, "character")
+  expect_length(result, 0)
+  expect_equal(result, character(0))
+})
+
+test_that("seq2nt: Didn't throw error for multiple sequences", {
+  expect_error(seq2nt(c("ATGC", "CGTA")), "There should be only one sequence")
+})
