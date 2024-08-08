58f99549be1ded54453710ddb6af10e7b8bc9ce7
Anna Igolkina <igolkinaanna11@gmail.com>
2024-08-08 16:41:04 +0200

    simsearch blast

--- simsearch.sh
+++ simsearch.sh
@@ -192,7 +192,7 @@ for db_file in "${db_files[@]}"; do
         blastn  -db ${db_file_full} \
                 -query ${file_input} \
                 -out ${blast_res} \
-                -outfmt "6 qseqid qstart qend sstart send pident length sseqid" \
+                -outfmt "6 qseqid qstart qend sstart send pident length sseqid qlen slen" \
                 -perc_identity ${sim_threshold}
     fi
 
