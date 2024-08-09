c99e93a6733d8f7d59aace644c579658a3e4dab6
Anna Igolkina <igolkinaanna11@gmail.com>
2024-08-09 12:54:50 +0200

    simsearch combine

--- sim/sim_in_genome_combine.R
+++ sim/sim_in_genome_combine.R
@@ -71,7 +71,7 @@ colnames(mx.cnt) = acc.names
 
 
 # Save
-write.table(mx.cnt, paste0(output.file, '.total_', sim.cutoff, '.cnt'), 
+write.table(mx.cnt, paste0(output.file, '.total_', sim.cutoff, '.txt'), 
             quote = F, row.names = T, col.names = T, sep = '\t')
 
 
