ade8508bc3909fbaf7ef9dc3afd8749bd1b577ac
Anna Igolkina <igolkinaanna11@gmail.com>
2024-08-14 22:43:43 +0200

    merging second round

--- merge/merge_02_new_hits.R
+++ merge/merge_02_new_hits.R
@@ -72,6 +72,8 @@ res$id = as.numeric(sapply(rownames(res), function(s) strsplit(s, '\\|')[[1]][2]
 res$name = rownames(res)
 res$chr = grep("Chr", '', sapply(res$name, function(s) strsplit(s, '\\|')[[1]][3]))
 
+pokaz('here')
+
 # Remove singletons
 res = res[res$total >= copy.number,]
 
