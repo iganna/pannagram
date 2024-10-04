c229f312b13867d05cee5754ee949565a9775204
Anna Igolkina <igolkinaanna11@gmail.com>
2024-10-04 13:09:07 +0200

    cleanup trust positions

--- inst/pangen/comb_03_cleanup.R
+++ inst/pangen/comb_03_cleanup.R
@@ -57,8 +57,6 @@ if(!dir.exists(path.cons)) stop('Consensus folder doesn’t exist')
 # ***********************************************************************
 # ---- Combinations of chromosomes query-base to create the alignments ----
 
-
-
 s.pattern <- paste0("^", aln.type.comb, ".*")
 files <- list.files(path = path.cons, pattern = s.pattern, full.names = FALSE)
 pref.combinations = gsub(aln.type.comb, "", files)
@@ -95,116 +93,28 @@ loop.function <- function(s.comb,
   
   file.comb = paste0(path.cons, aln.type.comb, s.comb,'.h5')
   
+  idx.trust = h5read(file.comb, v.idx.trust)
+  idx.trust = idx.trust != 0
   groups = h5ls(file.comb)
   accessions = groups$name[groups$group == gr.accs.b]
-  
-  # Create group for blocks
-  suppressMessages({ h5createGroup(file.comb, gr.blocks) })
-  
-  idx.break = c()
+ 
   for(acc in accessions){
-    
-    pokaz('Accession', acc, 'combination', s.comb, file=file.log.loop, echo=echo.loop)
-    
-    v.init = h5read(file.comb, paste0(gr.accs.e, acc))
-    if(length(v.init) == 0) {
-      stop(sprintf("Skipping empty accession: %s\n", acc))
-    }
-    v = v.init
-    
-    
-    # ----  Find breaks  ----
-    
-    # Find blocks of additional breaks
-    v = cbind(v, 1:length(v))                       # column 2 - in ref-based coordinates
-    
-    v = v[!is.na(v[,1]),]                           # column 1 - existing coordinates of accessions
-    v = v[v[,1] != 0,]                              # column 1 - existing coordinates of accessions
-    
-    if(nrow(v) == 0) {
-      stop(sprintf("Skipping empty v for accession: %s\n", acc))
-    }
-    v = cbind(v, 1:nrow(v))                       # 3 - ranked order in ref-based coordinates
-    v = cbind(v, rank(abs(v[,1])) * sign(v[,1]))  # 4 - signed-ranked-order in accessions coordinates 
-    
-    # Save blocks
-    idx.block.tmp = which(abs(diff(v[,4])) != 1)
-    idx.block.df = data.frame(beg = v[c(1,idx.block.tmp+1), 2], end = v[c(idx.block.tmp, nrow(v)), 2])
-
-    v.block = rep(0, length(v.init))
-    for(i.bl in 1:nrow(idx.block.df)){
-      v.block[idx.block.df$beg[i.bl]:idx.block.df$end[i.bl]] = i.bl
-    }
-    
+    pokaz(acc)
+    s.acc = paste0(gr.accs.e, acc)
+    v = h5read(file.comb, s.acc)
+    v = v[idx.trust]
+      
     suppressMessages({
-      h5write(v.block, file.comb, paste0(gr.blocks, acc))
+      h5delete(file.comb, s.acc)
+      h5write(v, file.comb, s.acc)
     })
-
-    # v = v[order(v[,1]),]  # not necessary
-    
-    # with the absence, but neighbouring
-    idx.tmp = which( (abs(diff(v[,4])) == 1) &  # Neighbouring in accession-based order
-                       (abs(diff(abs(v[,3])) == 1)) &  # Neighbouring in ref-based order
-                       (abs(diff(v[,1])) <= max.len.gap) &  # Filtering by length in accession coordinates
-                       (abs(diff(v[,2])) <= max.len.gap) &  # Filtering by length in reference coordinates
-                       (abs(diff(v[,1])) > 1))  # NOT neighbouring in accession-specific coordinates
-    
-    # Fix (beg < end) order
-    if(length(idx.tmp) == 0) {
-      stop(sprintf("Skipping empty idx.tmp for accession: %s\n", acc))
-    }
-    idx.tmp.acc = data.frame(beg = v[idx.tmp,2], end = v[idx.tmp+1,2], acc = acc)
-    idx.ord = which(idx.tmp.acc$beg > idx.tmp.acc$end)
-    if(length(idx.ord) > 0){
-      tmp = idx.tmp.acc$beg[idx.ord]
-      idx.tmp.acc$beg[idx.ord] = idx.tmp.acc$end[idx.ord]
-      idx.tmp.acc$end[idx.ord] = tmp
-    }
-    # idx.tmp.acc = idx.tmp.acc[order(idx.tmp.acc$beg),]  # order ONLY if ordered before
-    
-    # Remove overlaps
-    idx.overlap = which( (idx.tmp.acc$beg[-1] - idx.tmp.acc$end[-nrow(idx.tmp.acc)]) <= 3)
-    
-    i.cnt = 0
-    if(length(idx.overlap) > 0){
-      j.ov = 0
-      for(i.ov in idx.overlap){
-        if(i.ov <= j.ov) next
-        j.ov = i.ov + 1
-        while(j.ov %in% idx.overlap){
-          j.ov = j.ov + 1
-        }
-        # print(c(i.ov, j.ov))
-        i.cnt = i.cnt + 1
-        idx.tmp.acc$end[i.ov] = idx.tmp.acc$end[j.ov]
-      }
-      idx.tmp.acc = idx.tmp.acc[-(idx.overlap+1),]
-    }
-    
-    
-    # Save breaks
-    idx.break = rbind(idx.break, idx.tmp.acc)
-    
-    rmSafe(x.corr)
-    rmSafe(x)
-    rmSafe(v)
-    rmSafe(v.init)
-    rmSafe(idx.tmp.acc)
-    rmSafe(idx.break.acc)
     
   }
-  
-  file.breaks = paste0(path.cons, 'breaks_', s.comb,'.rds')
-  saveRDS(idx.break, file.breaks)
-  
-  rmSafe(idx.break)
-  
-  H5close()
-  gc()
   
   pokaz('Done.', file=file.log.loop, echo=echo.loop)
   return(NULL)
 }
+  
 
 
 # ***********************************************************************
