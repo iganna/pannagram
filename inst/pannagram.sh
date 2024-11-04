6cb93201acdcbe849f9b793ba9fda1e52d21a839
Anna Igolkina <igolkinaanna11@gmail.com>
2024-11-04 16:40:27 +0100

    unique gaps

--- inst/pannagram.sh
+++ inst/pannagram.sh
@@ -359,42 +359,42 @@ elif [ ! -z "${nchr}" ] && [ -z "${nchr_ref}" ]; then  # nchr is defined.
     nchr_ref=${nchr}
 fi
 
-# Check the number of chromosomes in the reference genome
-if [[ ${nchr_ref} -ne 0 ]]; then
-    for ref_name in "${refs_all[@]}"; do
-        for ext in "${genome_extensions[@]}"; do
-            # Find the file with the current acc and extension
-            file="${path_ref}/${ref_name}.${ext}"
-            # Check if the file exists
-            if [[ -f "$file" ]]; then
-                count=$(grep -c '^>' "$file")
-                if [[ ${count} -lt ${nchr_ref} ]]; then
-                    pokaz_error "Error: Number of chromosomes in the reference genome ${ref_name} is ${count}. Must be at least ${nchr_ref}."
-                    exit 1
-                fi
-            fi
-        done
-    done
-fi
+# # Check the number of chromosomes in the reference genome
+# if [[ ${nchr_ref} -ne 0 ]]; then
+#     for ref_name in "${refs_all[@]}"; do
+#         for ext in "${genome_extensions[@]}"; do
+#             # Find the file with the current acc and extension
+#             file="${path_ref}/${ref_name}.${ext}"
+#             # Check if the file exists
+#             if [[ -f "$file" ]]; then
+#                 count=$(grep -c '^>' "$file")
+#                 if [[ ${count} -lt ${nchr_ref} ]]; then
+#                     pokaz_error "Error: Number of chromosomes in the reference genome ${ref_name} is ${count}. Must be at least ${nchr_ref}."
+#                     exit 1
+#                 fi
+#             fi
+#         done
+#     done
+# fi
 
-# Check the number of chromosomes in the accessions genome
-if [[ ${nchr} -ne 0 ]]; then
-    for acc_name in "${acc_set[@]}"; do
-        for ext in "${genome_extensions[@]}"; do
-            # Find the file with the current acc and extension
-            file="${path_in}/${acc_name}.${ext}"
+# # Check the number of chromosomes in the accessions genome
+# if [[ ${nchr} -ne 0 ]]; then
+#     for acc_name in "${acc_set[@]}"; do
+#         for ext in "${genome_extensions[@]}"; do
+#             # Find the file with the current acc and extension
+#             file="${path_in}/${acc_name}.${ext}"
             
-            # Check if the file exists
-            if [[ -f "$file" ]]; then
-                count=$(grep -c '^>' "$file")
-                if [[ ${count} -lt ${nchr} ]]; then
-                    pokaz_error "Error: Number of chromosomes in the reference genome ${acc_name} is ${count}. Must be at least ${nchr}."
-                    exit 1
-                fi
-            fi
-        done
-    done
-fi
+#             # Check if the file exists
+#             if [[ -f "$file" ]]; then
+#                 count=$(grep -c '^>' "$file")
+#                 if [[ ${count} -lt ${nchr} ]]; then
+#                     pokaz_error "Error: Number of chromosomes in the reference genome ${acc_name} is ${count}. Must be at least ${nchr}."
+#                     exit 1
+#                 fi
+#             fi
+#         done
+#     done
+# fi
 
 
 
