c49da054aa4a39ca9cd4ea4eae6dd6f71ff65d37
Anna Igolkina <igolkinaanna11@gmail.com>
2024-11-11 12:45:39 +0100

    test memory

--- inst/pangen/synteny_03_get_gaps.R
+++ inst/pangen/synteny_03_get_gaps.R
@@ -139,9 +139,9 @@ loop.function <- function(f.maj,
   base.len = length(base.fas.bw)
   pokaz('Length of base:', base.len, file=file.log.loop, echo=echo.loop)
   
-  pokaz(memory.size())
   gc()
-  pokaz(memory.size())
+  pokaz('check')
+  Sys.sleep(10) 
   
   # Read query sequences
   query.file = paste0(acc, '_chr',query.chr, '.fasta')
@@ -152,11 +152,9 @@ loop.function <- function(f.maj,
   query.len = length(query.fas.chr)
   pokaz('Length of query:', query.len, file=file.log.loop, echo=echo.loop)
   
-  pokaz(memory.size())
+
   gc()
-  pokaz(memory.size())
-  
-  
+  pokaz('check')
   Sys.sleep(600) 
   
   x = readRDS(paste0(path.aln, f.maj))
