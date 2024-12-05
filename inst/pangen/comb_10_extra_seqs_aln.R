217fe088d362806326eeceed6d869fae60051cc5
Anna Igolkina <igolkinaanna11@gmail.com>
2024-12-05 19:57:56 +0100

    fix the pattern

--- inst/pangen/comb_10_extra_seqs_aln.R
+++ inst/pangen/comb_10_extra_seqs_aln.R
@@ -85,7 +85,7 @@ if (!dir.exists(path.work)) {
 # **************************************************************************
 # ---- Combinations of chromosomes query-base to create the alignments ----
 
-s.pattern <- paste0("^", aln.type.in, ".*")
+s.pattern <- paste0("^", aln.type.in, "\\d+_\\d+\\.h5$")
 files <- list.files(path = path.cons, pattern = s.pattern, full.names = FALSE)
 pref.combinations = gsub(aln.type.in, "", files)
 pref.combinations <- sub(".h5", "", pref.combinations)
