99235ab49a11b9f585c5f08e25b7adc40e72e507
Anna Igolkina <igolkinaanna11@gmail.com>
2025-02-25 13:46:46 +0100

    ntplot

--- inst/visualisation/ntplot.R
+++ inst/visualisation/ntplot.R
@@ -41,7 +41,8 @@ ntplot <- function(sequence, wnd=100, nt.separate = F) {
   
   p = ggplot(df.freq, aes(x = Position, y = Frequency, fill = Base)) +
     geom_bar(stat = "identity", width = 1) +
-    theme_minimal()+ scale_fill_manual(values = msa.cols)
+    theme_minimal()+ scale_fill_manual(values = msa.cols) +
+    theme(legend.position = "bottom", legend.direction = "horizontal")
   
   if(nt.separate){
     p <- p + facet_wrap(~Base, scales = "free_y", ncol = 1) + theme(legend.position = "none") 
