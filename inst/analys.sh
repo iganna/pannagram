f307a6ef141bb4b1c48e05602a1d18b00dc24750
iganna <igolkinaanna11@gmail.com>
2025-05-14 12:04:34 +0200

    pi diversity

--- inst/analys.sh
+++ inst/analys.sh
@@ -179,12 +179,12 @@ fi
 
 if [ "$run_snp" = true ]; then
     pokaz_stage "Get SNPs."
-    Rscript $INSTALLED_PATH/analys/analys_04_snp.R \
-        --path.cons ${path_consensus} \
-        --ref.pref  ${ref_pref} \
-        --path.chr ${path_chromosomes} \
-        --aln.type ${aln_type} \
-        --cores ${cores}
+    # Rscript $INSTALLED_PATH/analys/analys_04_snp.R \
+    #     --path.cons ${path_consensus} \
+    #     --ref.pref  ${ref_pref} \
+    #     --path.chr ${path_chromosomes} \
+    #     --aln.type ${aln_type} \
+    #     --cores ${cores}
 
     # ---------------
     # Pi divirsity
@@ -214,9 +214,9 @@ if [ "$run_snp" = true ]; then
 
             plink --vcf "${vcf_file}" --distance  --out "${vcf_file}.dist" --allow-extra-chr
 
-            Rscript $INSTALLED_PATH/analys/analys_04_snp_dist.R \
-               --path.figures ${path_plots} \
-               --file.pi "${vcf_file}"
+            # Rscript $INSTALLED_PATH/analys/analys_04_snp_dist.R \
+            #    --path.figures ${path_plots} \
+            #    --file.pi "${vcf_file}"
 
         done
     fi
