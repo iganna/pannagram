de0452526a76fefaf9cec0a4a70769c817e623fb
Alex <username@users.noreply.github.com>
2025-06-15 22:20:02 +0300

    allgram: aligned argparse and help to latest changes

--- inst/utils/argparse_pannagram.sh
+++ inst/utils/argparse_pannagram.sh
@@ -26,46 +26,49 @@ required_params=()
 # Process command-line arguments
 while [ $# -gt 0 ]; do
     case "$1" in
-        -h| -help )       print_usage_detailed; print_examples; exit 0 ;;
-        -s|-stage|-step ) step_start="$2"; shift 2 ;;
-        -e | -end )       step_end="$2"; shift 2 ;;
-        -log)             log_level="$2"; shift 2 ;;
-        -cores)           cores="$2"; shift 2 ;;
-        -clean | -cleanup)clean="T"; shift 1 ;;
-        -one_step | -1s)  one_step="T"; shift 1 ;;
-        -rm_inter)        rm_inter="T"; shift 1 ;;
+        -h|-help )       print_usage_detailed; print_examples; exit 0 ;;
         
         # Required parameters
-        -path_out)        path_out="$2"; shift 2; required_params+=("path_out") ;;
-        -path_in)         path_in="$2"; shift 2; required_params+=("path_in") ;;
+        -path_out|-path_project) path_project="$2"; shift 2; required_params+=("path_project") ;;
+        -path_in)                path_in="$2"; shift 2; required_params+=("path_in") ;; # path with all genomes in fasta format
+
+        -s|-stage|-step) step_start="$2"; shift 2 ;; # first stage to run from, when the stage is not provided - the last interrupted stage withh be re-run
+        -e|-end)         step_end="$2"; shift 2 ;;   # last stage to run to
+        -log)            log_level="$2"; shift 2 ;;
+        -cores)          cores="$2"; shift 2 ;;
+        -clean|-cleanup) clean="T"; shift 1 ;;
+        -one_step | -1s) one_step="T"; shift 1 ;;
+        -rm_inter)       rm_inter="T"; shift 1 ;;
+        
         
         # REF-based
-        -pre)             mode_pre="T"; shift 1 ;;
-        -ref)             ref_name="$2"; mode_ref="T"; shift 2 ;;
-        -path_ref)        path_ref="$2"; shift 2 ;;
+        -pre)            mode_pre="T"; shift 1 ;;                # shitch to the preliminary mode
+        -ref)            ref_name="$2"; mode_ref="T"; shift 2 ;; # name of the reference genome
+        -path_ref)       path_ref="$2"; shift 2 ;;               # dont provide if it's the same folder as the path with query genomes
         
         # MSA
-        -refs)            ref_set="$2"; mode_msa="T"; shift 2 ;;
-        -nref)            ref_num="$2"; mode_msa="T"; shift 2 ;;
+        -refs)           ref_set="$2"; mode_msa="T"; shift 2 ;;
+        -nref)           ref_num="$2"; mode_msa="T"; shift 2 ;;
         
         # Number of chromosomes
-        -nchr)            nchr="$2"; shift 2 ;;
-        -nchr_ref)        nchr_ref="$2"; shift 2 ;;
+        -nchr)           nchr="$2"; shift 2 ;;        # in every genome
+        -nchr_ref)       nchr_ref="$2"; shift 2 ;;    # in reference genome
 
-        -part_len)        part_len="$2"; shift 2 ;;
-        -p_ident)         p_ident="$2"; shift 2 ;;
-        -p_ident_gap)     p_ident_gap="$2"; shift 2 ;;
-        -max_len_gap)     max_len_gap="$2"; shift 2 ;;
-        -one2one)         one2one="T"; shift 1 ;;
-        -all2all)         one2one="F"; shift 1 ;;
-        -incl_reps |-include_repeats ) purge_reps="F"; shift 1 ;;
-        -rev )            flag_rev="T"; shift 1 ;;
-        -orf )            flag_orf="T"; shift 1 ;;
-        -purge_contigs)   purge_contigs="T"; shift 1 ;;
-        -accessions)      acc_file="$2"; shift 2 ;;
-        -combinations)    comb_file="$2"; shift 2 ;;
-        -extra)           extra_steps="T"; shift 1 ;;
-        *)                unrecognized_options+=("$1"); shift 1 ;;
+        -part_len)       part_len="$2"; shift 2 ;;    # fragments to which each chromosome should be cut, has a default value 5000
+        -p_ident)        p_ident="$2"; shift 2 ;;     # percent of identity
+        -p_ident_gap)    p_ident_gap="$2"; shift 2 ;; # percent of identity of gaps
+        -max_len_gap)    max_len_gap="$2"; shift 2 ;; # Max length that can be aligned with MAFFT
+
+        -one2one)        one2one="T"; shift 1 ;;                 # do compare chroms one-to-one (default in REF and MSA modes)
+        -all2all)        one2one="F"; shift 1 ;;                 # do compare chroms all-to-all (default in PRE mode)
+        -incl_reps |-include_repeats) purge_reps="F"; shift 1 ;; # repeats filtration
+        -rev )           flag_rev="T"; shift 1 ;;                # reverse parts
+        -orf )           flag_orf="T"; shift 1 ;;                # run ORF finder
+        -purge_contigs)  purge_contigs="T"; shift 1 ;;
+        -accessions)     acc_file="$2"; shift 2 ;;               # accessions file to analyze
+        -combinations)   comb_file="$2"; shift 2 ;;              # chromosomal combinations tsv file to analyze: first column - query, second column - reference(base)
+        -extra)          extra_steps="T"; shift 1 ;;             # perform extra steps
+        *)               unrecognized_options+=("$1"); shift 1 ;;
     esac
 done
 
@@ -108,13 +111,7 @@ if [[ -n "$one2one" && "$one2one" != "T" && "$one2one" != "F" ]]; then
 fi
 
 path_in=$(add_symbol_if_missing "$path_in" "/")
-path_out=$(add_symbol_if_missing "$path_out" "/")
+path_project=$(add_symbol_if_missing "$path_project" "/")
 
 path_ref="${path_ref:-${path_in}}"
 path_ref=$(add_symbol_if_missing "$path_ref" "/")
-
-# Export variables for main script
-export mode_pre mode_ref mode_msa clean one_step purge_reps rm_inter extra_steps purge_contigs
-export step_start step_end log_level cores path_out path_in path_ref ref_name ref_set ref_num
-export nchr nchr_ref part_len p_ident p_ident_gap max_len_gap one2one flag_rev flag_orf acc_file comb_file
-export name_mode_pre name_mode_ref name_mode_msa mode_pangen
