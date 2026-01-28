a75743f7141e2a5c8b84b6e8b5d880a9e44d72c0
iganna <igolkinaanna11@gmail.com>
2026-01-28 18:44:37 +0100

    add kostyl

--- inst/pangen/comb_new_07_final_aln.R
+++ inst/pangen/comb_new_07_final_aln.R
@@ -88,7 +88,7 @@ stat.comb <- data.frame(comb = character(),
                         coverage = numeric(),
                         stringsAsFactors = FALSE)
 
-pref.combinations = '4_4'
+pref.combinations = '5_5'
 for(s.comb in pref.combinations){
   
   # Log files
