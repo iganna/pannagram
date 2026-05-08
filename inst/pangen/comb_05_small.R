d788791a4c37a6ab62764cfb51063240d58996a6
iganna <igolkinaanna11@gmail.com>
2026-05-08 11:16:24 +0200

    step 15 errorr message fix

--- inst/pangen/comb_05_small.R
+++ inst/pangen/comb_05_small.R
@@ -31,6 +31,8 @@ option_list <- list(
 opt_parser = OptionParser(option_list=option_list);
 opt = parse_args(opt_parser, args = args);
 
+pokaz(opt)
+
 # ***********************************************************************
 # ---- Logging ----
 
